501ef27a6b049be703d0a803c2f29a7963178006
indexzero <charlie.robbins@gmail.com>
2014-07-17 16:54:58 -0400

    [dist] Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+2014.empirenode.org
+=================
+
+Public site for EmpireNode 2014
---
+++ server.js
@@ -0,0 +1,35 @@
+var union    = require('union'),
+    ecstatic = require('ecstatic');
+
+var is2014 = /2014\./;
+
+//
+// ### redirect(res)
+// World's simplest redirect function
+//
+function redirect(res) {
+  var url  = 'http://2014.empirejs.org',
+      body = '<p>301. Redirecting to <a href="' + url + '">' + url + '</a></p>';
+
+  res.writeHead(301, { 'content-type': 'text/html', location: url });
+  res.end(body);
+}
+
+var server = union.createServer({
+  before: [
+    function (req, res) {
+      var host = req.headers.host;
+      if (process.env.NODE_ENV === 'production' && !is2014.test(host)) {
+        return redirect(res);
+      }
+
+      res.emit('next');
+    },
+    ecstatic(__dirname + '/public'),
+    function (req, res) {
+      return redirect(res);
+    }
+  ]
+});
+
+server.listen(8080);
