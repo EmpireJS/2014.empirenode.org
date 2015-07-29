d7c58996ce88d2caf61681ad485927c7e91b9f22
indexzero <charlie.robbins@gmail.com>
2015-07-29 01:07:11 -0700

    [fix] Redirect to `2015.empirenode.org`.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var union    = require('union'),
     ecstatic = require('ecstatic');
 
-var is2014 = /2014\./;
+var is2014 = /2015\./;
 
 //
 // ### redirect(res)
 // World's simplest redirect function
 //
 function redirect(res) {
-  var url  = 'http://2014.empirenode.org',
+  var url  = 'http://2015.empirenode.org',
       body = '<p>301. Redirecting to <a href="' + url + '">' + url + '</a></p>';
 
   res.writeHead(301, { 'content-type': 'text/html', location: url });
@@ -17,14 +17,14 @@ function redirect(res) {
 
 var server = union.createServer({
   before: [
-    // function (req, res) {
-    //   var host = req.headers.host;
-    //   if (process.env.NODE_ENV === 'production' && !is2014.test(host)) {
-    //     return redirect(res);
-    //   }
+    function (req, res) {
+      var host = req.headers.host;
+      if (process.env.NODE_ENV === 'production' && !is2015.test(host)) {
+        return redirect(res);
+      }
 
-    //   res.emit('next');
-    // },
+      res.emit('next');
+    },
     ecstatic({
       root: __dirname + '/public',
       defaultExt: true
